df26e765039acaef2c8942c161f912835ced32de
haroldrochaucaldastic <haroldrocha.tic@ucaldas.edu.co>
2022-11-03 19:58:44 -0500

    cambio index modelo tienda caja y producto

--- backend-tienda/src/models/index.ts
+++ backend-tienda/src/models/index.ts
@@ -1,3 +1,6 @@
 export * from './usuario.model';
 export * from './administrador.model';
 export * from './empleado.model';
+export * from './tienda.model';
+export * from './caja.model';
+export * from './producto.model';
