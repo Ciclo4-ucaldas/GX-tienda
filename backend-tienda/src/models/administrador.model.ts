a5a68f6aebe09ebfe02489027a7e9128ea539842
haroldrochaucaldastic <haroldrocha.tic@ucaldas.edu.co>
2022-11-03 19:52:26 -0500

    creacion de modelo administrador

---
+++ backend-tienda/src/models/administrador.model.ts
@@ -0,0 +1,29 @@
+import {model, property} from '@loopback/repository';
+import {Usuario} from '.';
+
+@model()
+export class Administrador extends Usuario {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  cargo: string;
+
+
+  constructor(data?: Partial<Administrador>) {
+    super(data);
+  }
+}
+
+export interface AdministradorRelations {
+  // describe navigational properties here
+}
+
+export type AdministradorWithRelations = Administrador & AdministradorRelations;
